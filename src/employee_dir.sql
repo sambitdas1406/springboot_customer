0a3f5ab61e4a47935a967845f978ac74e71ecb91
USER <sambitdas55@gmail.com>
2019-01-04 21:51:25 +0530

    first deploy

---
+++ src/employee_dir.sql
@@ -0,0 +1,32 @@
+/*
+SQLyog - Free MySQL GUI v5.02
+Host - 5.1.69-community : Database - employee_directory
+*********************************************************************
+Server version : 5.1.69-community
+*/
+
+
+create database if not exists `employee_directory`;
+
+USE `employee_directory`;
+
+/*Table structure for table `employee` */
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `employee` */
+
+insert into `employee` values 
+(1,'Leslie','Andrews','leslie@luv2code.com'),
+(2,'Emma','Baumgarten','emma@luv2code.com'),
+(3,'Avani','Gupta','avani@luv2code.com'),
+(4,'Yuri','Petrov','yuri@luv2code.com'),
+(5,'Juan','Vega','juan@luv2code.com');
